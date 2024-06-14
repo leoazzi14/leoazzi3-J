6f44eb763ec30567a8427ebd7a1da552045ab0cb
leoazzi14 <110132974+leoazzi14@users.noreply.github.com>
2024-06-14 09:43:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes  = document.querySelectorAll(".botao");
 const conteudo  = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+ tempo[0]. textContent = "Olá";
+const tempoObjetivo1 = new Date(2007,06);
+const tempoObjetivo2 = new Date(2024,06);
+const agora = new Date();
+tempo[1].  textContent = agora
 for (let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
     for (j=0;j<botoes.length;j++){
